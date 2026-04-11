797180c218ddcaddc4ec61fad95eb28f5a9a2224
Vikash kumar <vikashkr072003@gmail.com>
2026-04-11 19:30:15 +0000

    move_zero

---
+++ array_question/question_01.py
@@ -0,0 +1,5 @@
+# ------Question_01 Solved-----
+# move_Zero :- Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+
+# solved
