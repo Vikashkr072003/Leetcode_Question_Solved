aa3974a5fe118266b4c661b06a98c5931190d5d5
Vikash kumar <vikashkr072003@gmail.com>
2026-04-17 21:39:47 +0000

    edit_in_move_to_zero

--- array_question/question_01.py
+++ array_question/question_01.py
@@ -3,22 +3,21 @@
 
 
 # Solved :-------
-nums = [0, 1, 0, 3, 12]
 
 
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        result = [0] * n
-        insert_pos = 0
+def moveToZero(nums):
+    j = 0
 
-        for num in nums:
-            if num != 0:
-                result[insert_pos] = num
-                insert_pos += 1
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[j] = nums[i]
+            j += 1
 
-        for i in range(n):
-            nums[i] = result[i]
+    for i in range(j, len(nums)):
+        nums[i] = 0
+
+    return nums
+
+
+arr = [0, 2, 0, 4]
+print(moveToZero(arr))
